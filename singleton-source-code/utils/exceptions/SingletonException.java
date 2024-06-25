8419830b7e3ecb2c9a849b9d9fa05c10cdcf43b3
Mauricio B. Avellar <mbavellar@gmail.com>
2024-06-25 15:40:07 -0300

    Delete singleton-source-code/utils directory

--- singleton-source-code/utils/exceptions/SingletonException.java
+++
@@ -1,7 +0,0 @@
-package com.codingwithfox.exceptions;
-
-public class SingletonException extends RuntimeException {
-  public SingletonException(String errorMsg) {
-    super(errorMsg);
-  }
-}
--- singleton-source-code/utils/exceptions/UnimplementedMethodException.java
+++
@@ -1,7 +0,0 @@
-package com.codingwithfox.exceptions;
-
-public class UnimplementedMethodException extends RuntimeException {
-  public UnimplementedMethodException(String errorMessage) {
-    super(errorMessage);
-  }
-}
